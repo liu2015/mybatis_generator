c30fa6fc48318fb31dae4e038b1bf4e614c1fa3f
os <liufuling14@163.com>
2019-09-22 23:40:43 +0800

    mybatis自动生成示例22 加入了日志示例 更新

--- src/main/java/com/example/demo/controller/usercontroller.java
+++ src/main/java/com/example/demo/controller/usercontroller.java
@@ -3,12 +3,14 @@ package com.example.demo.controller;
 import com.example.demo.entity.User;
 import com.example.demo.service.userservice;
 import org.apache.ibatis.annotations.SelectProvider;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import tk.mybatis.mapper.provider.base.BaseSelectProvider;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 @Controller
 public class usercontroller {
@@ -24,6 +26,12 @@ public String login(){
 @RequestMapping("/index")
     @SelectProvider(type = BaseSelectProvider.class, method = "dynamicSQL")
     public String select(User user) {
+
+  Logger logger= (Logger) LoggerFactory.getLogger(this.getClass());
+
+  logger.info("这是info级别");
+
+
 
   System.out.println(user.toString());
     List dd= (List) userservice.select(user);
